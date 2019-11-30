74ca61edac26348e611c0afd13f60963d37e31fd
yawuplus <1397991131@qq.com>
2019-11-30 10:30:03 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,44 +1,66 @@
+# coding:utf-8
 from setuptools import setup, find_packages
+
 
 setup(
     name='typeidea',
-    version='0.1',
+    version='${version}',
     description='Blog System base on Django',
-    author='minicloudsky',
+    author='the5fire',
     author_email='minicloudsky@gmail.com',
-    url='https://www.yawujia.cn',
+    url='https://www.yawujia.com',
     license='MIT',
     packages=find_packages('typeidea'),
     package_dir={'': 'typeidea'},
-    package_data={'': [
-        'themes/*/*/*//*',
-    ]},
-    # include_package_data=True, #方法二: 配合MANIFEST.in文件
-    install_requires=[
+    # package_data={'': [    # 打包数据文件，方法一
+        # 'themes/*/*/*/*',  # 需要按目录层级匹配
+    # ]},
+    include_package_data=True,  # 方法二 配合 MANIFEST.in 文件
+     install_requires=[
         'django~=1.11',
+        'gunicorn==19.8.1',
+        'supervisor==4.0.0dev0',
+        'xadmin==0.6.1',
+        'mysqlclient==1.3.12',
+        'django-ckeditor==5.4.0',
+        'django-rest-framework==0.1.0',
+        'django-redis==4.8.0',
+        'django-autocomplete-light==3.2.10',
+        'mistune==0.8.3',
+        'Pillow==4.3.0',
+        'coreapi==2.3.3',
+        'django-redis==4.8.0',
+        'hiredis==0.2.0',
+        # debug
+        'django-debug-toolbar==1.9.1',
+        'django-silk==2.0.0',
     ],
-    extra_require={
-        'ipython': ['ipython==6.2.1']
-    },
     scripts=[
-        './manage.py',
+        'typeidea/manage.py',
+        'typeidea/typeidea/wsgi.py',
     ],
     entry_points={
         'console_scripts': [
             'typeidea_manage = manage:main',
         ]
     },
-    classifiers=[
-        # 软件成熟度
-        # 3 -Alpha 4 -beta 5 - Production/Stable
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-        # 指明项目的受众
+
+        # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic  :: Software Development :: Libraries',
-        # 选择项目的许可证(license)
+        'Topic :: Blog :: Django Blog',
+
+        # Pick your license as you wish
         'License :: OSI Approved :: MIT License',
-        # 指定项目需要的Python版本
-        'Programming Language :: Python 3.6',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
     ],
 
 )
